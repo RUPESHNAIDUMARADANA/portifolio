be956f78566cabb2c1c4e97267efda97461fea97
RUPESHNAIDUMARADANA <139362394+RUPESHNAIDUMARADANA@users.noreply.github.com>
2024-05-20 15:34:08 +0530

    Create app.js

---
+++ assets/js/app.js
@@ -0,0 +1,72 @@
+particlesJS('particles-js', {
+  "particles": {
+    "number": {
+      "value": 250,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#000000"
+    },
+    "shape": {
+      "type": "star",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 5
+      }
+    },
+    "opacity": {
+      "value": 0.5
+    },
+    "size": {
+      "value": 5,
+      "random": true
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#000080",
+      "opacity": 1,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 6,
+      "out_mode": "out"
+    }
+  },
+  "interactivity": {
+    "detect_on": "window",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "repulse"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "bubble"
+      },
+      "resize": true
+    },
+    "modes": {
+      "bubble": {
+        "distance": 400,
+        "size": 10,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 6
+      },
+      "repulse": {
+        "distance": 200
+      }
+    }
+  },
+  "retina_detect": true
+});
+
+
